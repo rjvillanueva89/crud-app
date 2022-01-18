624683607bd21248f4ed87dbc52742b3577677c5
Rj Villanueva <rj.villanueva89@gmail.com>
2022-01-19 05:00:08 +0800

    - added json-server/db.json - added table - added fetch people get request

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,16 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <>
+      <Head>
+        <title>CRUD App</title>
+        <meta name="description" content="Why you gotta be so CRUD!" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </>
 }
 
 export default MyApp
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,89 @@
+import type { NextPage } from 'next'
+import { useEffect } from 'react'
+
 const Home: NextPage = () => {
+
+  useEffect(() => {
+    // fetch data from json-server/db.json
+    fetch('http://localhost:3001/people', {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      }
+    })
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+    });
+  });
+
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <>
+      <div className="bg-gray-100 font-family-karla w-full">
+        <table className="w-full bg-white">
+          <thead className="bg-gray-800 text-white">
+            <tr>
+              <th className="w-1/3 text-left py-3 px-4 uppercase font-semibold text-sm">Name</th>
+              <th className="w-1/3 text-left py-3 px-4 uppercase font-semibold text-sm">Last name</th>
+              <th className="text-left py-3 px-4 uppercase font-semibold text-sm">Phone</th>
+              <th className="text-left py-3 px-4 uppercase font-semibold text-sm">Email</th>
+            </tr>
+          </thead>
+          <tbody className="text-gray-700">
+            <tr>
+              <td className="w-1/3 text-left py-3 px-4">Lian</td>
+              <td className="w-1/3 text-left py-3 px-4">Smith</td>
+              <td className="text-left py-3 px-4"><a className="hover:text-blue-500" href="tel:622322662">622322662</a></td>
+              <td className="text-left py-3 px-4"><a className="hover:text-blue-500" href="mailto:jonsmith@mail.com">jonsmith@mail.com</a></td>
+            </tr>
+            <tr className="bg-gray-200">
+              <td className="w-1/3 text-left py-3 px-4">Emma</td>
+              <td className="w-1/3 text-left py-3 px-4">Johnson</td>
+              <td className="text-left py-3 px-4"><a className="hover:text-blue-500" href="tel:622322662">622322662</a></td>
+              <td className="text-left py-3 px-4"><a className="hover:text-blue-500" href="mailto:jonsmith@mail.com">jonsmith@mail.com</a></td>
+            </tr>
+            <tr>
+              <td className="w-1/3 text-left py-3 px-4">Oliver</td>
+              <td className="w-1/3 text-left py-3 px-4">Williams</td>
+              <td className="text-left py-3 px-4"><a className="hover:text-blue-500" href="tel:622322662">622322662</a></td>
+              <td className="text-left py-3 px-4"><a className="hover:text-blue-500" href="mailto:jonsmith@mail.com">jonsmith@mail.com</a></td>
+            </tr>
+            <tr className="bg-gray-200">
+              <td className="w-1/3 text-left py-3 px-4">Isabella</td>
+              <td className="w-1/3 text-left py-3 px-4">Brown</td>
+              <td className="text-left py-3 px-4"><a className="hover:text-blue-500" href="tel:622322662">622322662</a></td>
+              <td className="text-left py-3 px-4"><a className="hover:text-blue-500" href="mailto:jonsmith@mail.com">jonsmith@mail.com</a></td>
+            </tr>
+            <tr>
+              <td className="w-1/3 text-left py-3 px-4">Lian</td>
+              <td className="w-1/3 text-left py-3 px-4">Smith</td>
+              <td className="text-left py-3 px-4"><a className="hover:text-blue-500" href="tel:622322662">622322662</a></td>
+              <td className="text-left py-3 px-4"><a className="hover:text-blue-500" href="mailto:jonsmith@mail.com">jonsmith@mail.com</a></td>
+            </tr>
+            <tr className="bg-gray-200">
+              <td className="w-1/3 text-left py-3 px-4">Emma</td>
+              <td className="w-1/3 text-left py-3 px-4">Johnson</td>
+              <td className="text-left py-3 px-4"><a className="hover:text-blue-500" href="tel:622322662">622322662</a></td>
+              <td className="text-left py-3 px-4"><a className="hover:text-blue-500" href="mailto:jonsmith@mail.com">jonsmith@mail.com</a></td>
+            </tr>
+            <tr>
+              <td className="w-1/3 text-left py-3 px-4">Oliver</td>
+              <td className="w-1/3 text-left py-3 px-4">Williams</td>
+              <td className="text-left py-3 px-4"><a className="hover:text-blue-500" href="tel:622322662">622322662</a></td>
+              <td className="text-left py-3 px-4"><a className="hover:text-blue-500" href="mailto:jonsmith@mail.com">jonsmith@mail.com</a></td>
+            </tr>
+            <tr className="bg-gray-200">
+              <td className="w-1/3 text-left py-3 px-4">Isabella</td>
+              <td className="w-1/3 text-left py-3 px-4">Brown</td>
+              <td className="text-left py-3 px-4"><a className="hover:text-blue-500" href="tel:622322662">622322662</a></td>
+              <td className="text-left py-3 px-4"><a className="hover:text-blue-500" href="mailto:jonsmith@mail.com">jonsmith@mail.com</a></td>
+            </tr>
+          </tbody>
+        </table>
+
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/js/all.min.js" integrity="sha256-KzZiKy0DWYsnwMF+X1DvQngQ2/FxF7MF3Ff72XcpuPs=" crossOrigin="anonymous"></script>
+      </div>
+    </>
   )
 }
 
