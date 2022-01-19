d15706582d100e507c4248a3389cf1f1b46e967b
Rj Villanueva <rj.villanueva89@gmail.com>
2022-01-19 09:54:52 +0800

    - added layout and components dir - added global layout - added table component - updated fetch users data

---
+++ layout/index.tsx
@@ -0,0 +1,28 @@
+import React, { ReactNode } from 'react'
+
+type Props = {
+  children?: ReactNode
+}
+
+const Layout = ({ children }: Props) => (
+  <div id="container" className="bg-gray-100 font-family-karla w-full min-h-screen">
+    <header id="nav" className="flex">
+      <div id="app-title" className="flex-none p-2">
+        <h1 className="font-bold">CRUD APP</h1>
+      </div>
+      <div id="action" className="flex-1 text-right">
+        <button className="px-3 py-2">
+          <i className="fa fa-plus"></i>
+        </button>
+      </div>
+    </header>
+
+    <main id="content">
+      {children}
+    </main>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/js/all.min.js" integrity="sha512-Tn2m0TIpgVyTzzvmxLNuqbSJH3JP8jm+Cy3hvHrW7ndTDcJ1w5mBiksqDBb8GpE2ksktFvDB/ykZ0mDpsZj20w==" crossOrigin="anonymous"></script>
+  </div>
+)
+
+export default Layout
--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    BASE_URL: process.env.BASE_URL,
+    DB_URL: process.env.DB_URL
+  }
 }
 
 module.exports = nextConfig
